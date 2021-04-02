55e9092296594e68502d851366183086286beaaa
ipvdevs <ipvdevs@gmail.com>
2021-04-02 11:47:07 +0300

    Modify pair to read-only

--- MySTL/Pair/Pair.hpp
+++ MySTL/Pair/Pair.hpp
@@ -1,41 +1,27 @@
 #ifndef PAIR__PAIR_HPP_
 #define PAIR__PAIR_HPP_
 
-// Class Pair must be read-only
+// Read-only class
 template<typename T1, typename T2>
 class Pair {
  private:
-  T1 first_type;
-  T2 second_type;
+  const T1 first_type;
+  const T2 second_type;
 
  public:
   Pair(const T1& first_type, const T2& second_type);
 
-  Pair<T1, T2>& operator=(const Pair&);
   bool operator==(const Pair&) const;
 
   T1 getFirstType() const;
   T2 getSecondType() const;
 
-  void swap(const Pair<T1, T2>& first_type, const Pair<T1, T2>& second_type);
-
+  Pair<T2, T1> swapPairTypes() const;
 };
 
 template<typename T1, typename T2>
-Pair<T1, T2>::Pair(const T1& first_type, const T2& second_type) {
-  this->first_type = first_type;
-  this->second_type = second_type;
-}
-
-template<typename T1, typename T2>
-Pair<T1, T2>& Pair<T1, T2>::operator=(const Pair& other) {
-  if (this != &other) {
-    this->first_type = other.first_type;
-    this->second_type = other.second_type;
-  }
-
-  return *this;
-}
+Pair<T1, T2>::Pair(const T1& first_type, const T2& second_type)
+    : first_type(first_type), second_type(second_type) {}
 
 template<typename T1, typename T2>
 bool Pair<T1, T2>::operator==(const Pair& other) const {
@@ -54,9 +40,7 @@ T2 Pair<T1, T2>::getSecondType() const {
 }
 
 template<typename T1, typename T2>
-void Pair<T1, T2>::swap(const Pair<T1, T2>& lhs, const Pair<T1, T2>& rhs) {
-  Pair<T1, T2> temp = lhs;
-  lhs = rhs;
-  rhs = temp;
+Pair<T2, T1> Pair<T1, T2>::swapPairTypes() const {
+  return Pair<T2, T1>(second_type, first_type);
 }
 #endif //PAIR__PAIR_HPP_
--- MySTL/Pair/main.cpp
+++ MySTL/Pair/main.cpp
@@ -2,19 +2,10 @@
 #include <iostream>
 
 int main() {
-  Pair<int, std::string> person(1, "Name");
-  Pair<int, std::string> anotherPerson(2, "Nombre");
-
-  std::cout << person.getFirstType() << ' ' << person.getSecondType() << '\n';
-
-  swap(person, anotherPerson);
-
-  std::cout << person.getFirstType() << ' ' << person.getSecondType() << '\n';
-
-  std::cout << "Equal: " << (person == anotherPerson ? "true" : "false") << '\n';
-  std::cout << "Assignment." << '\n';
-  anotherPerson = person;
-  std::cout << "Equal: " << (person == anotherPerson ? "true" : "false") << '\n';
+  auto person = Pair<int, std::string>(1, "Name");
+  std::cout << person.getFirstType() << ' ' << person.getSecondType() << std::endl;
+  auto swapped = person.swapPairTypes();
+  std::cout << swapped.getFirstType() << ' ' << swapped.getSecondType() << std::endl;
 
   return 0;
 }
